6cf3a57786f3351933264aae566541279d5401d4
Sainathr92 <sainathr92@gmail.com>
2026-08-01 20:08:29 +0530

    Added S3 Bucket and security group

--- main.tf
+++ main.tf
@@ -9,7 +9,45 @@ terraform {
 provider "aws" {
  region = "us-east-1"
 }
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0.0.0.0"]
+  }
+}
+
 resource "aws_instance" "free_tier" {
  ami = "ami-02b64aa047cb5edf5" # Amazon Linux Server 2023 LTS
  instance_type = "t3.micro" # Free Tier eligible
+ vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "saie-demo-bucket-20260801"
+
+  tags = {
+    Name        = "example-bucket"
+    Environment = "dev"
+  }
 }
